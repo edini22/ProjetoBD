53299a376353d0d9e23d3e79b863a59367e09b52
Eduardo22 <figueiredo92002@gmail.com>
2022-04-24 23:02:31 +0100

    algumas funcoes sql feitas para inserir

--- src/insert.sql
+++ src/insert.sql
@@ -1,5 +1,7 @@
 -- FUNCOES ==========================================================================
 
+--TODO: FAZER PROTECOES PARA VER SE ESSE PRODUTO AO INSERIR JA EXISTE!!!!
+
 -- Devolve o id do user dando o seu username
 CREATE OR REPLACE FUNCTION ID_USER(UN VARCHAR)
 RETURNS INT
@@ -11,6 +13,30 @@ BEGIN
     SELECT utilizadores.id INTO id FROM utilizadores
     WHERE utilizadores.username = UN;
     RETURN id;
+END;
+$$ LANGUAGE plpgsql;
+
+-- insere um comprador
+CREATE OR REPLACE FUNCTION ADD_COMPRADOR(userName VARCHAR,pass VARCHAR,Nome VARCHAR,Morada VARCHAR)
+RETURNS void
+AS
+$$
+DECLARE
+    BEGIN
+        INSERT INTO utilizadores (username, password, nome) VALUES (userName, pass, Nome);
+        INSERT INTO compradores (utilizador_id,morada) VALUES (ID_USER(userName),Morada);
+END;
+$$ LANGUAGE plpgsql;
+
+-- insere um admin
+CREATE OR REPLACE FUNCTION ADD_ADMIN(userName VARCHAR,pass VARCHAR,Nome VARCHAR)
+RETURNS void
+AS
+$$
+DECLARE
+    BEGIN
+        INSERT INTO utilizadores (username, password, nome) VALUES (userName, pass, Nome);
+        INSERT INTO admins (utilizador_id) VALUES (ID_USER(userName));
 END;
 $$ LANGUAGE plpgsql;
 
@@ -28,6 +54,19 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- insere um vendedor
+CREATE OR REPLACE FUNCTION ADD_VENDEDOR(userName VARCHAR,pass VARCHAR,Nome VARCHAR,Nif INTEGER)
+RETURNS void
+AS
+$$
+DECLARE
+    BEGIN
+        INSERT INTO utilizadores (username, password, nome) VALUES (userName, pass, Nome);
+        INSERT INTO vendedores (utilizador_id,nif) VALUES (ID_USER(userName),Nif);
+END;
+$$ LANGUAGE plpgsql;
+
+
 -- Devolve o id do produto dando o seu nome e o id do vendedor
 -- TODO: 
 CREATE OR REPLACE FUNCTION ID_PRODUTO(string VARCHAR, vendedor INT)
@@ -43,40 +82,43 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- insere um computador
+CREATE OR REPLACE FUNCTION ADD_COMPUTADOR(Nome VARCHAR,Descricao VARCHAR,Preco FLOAT,Stock INTEGER,nomeVendedor VARCHAR,Processador VARCHAR,Ram INTEGER,Rom INTEGER,Grafica VARCHAR)
+RETURNS void
+AS
+$$
+DECLARE
+    BEGIN
+        INSERT INTO produtos (nome, descricao, preco, stock, vendedor_id) VALUES (Nome, Descricao, Preco, Stock,ID_VENDEDOR(nomeVendedor));
+        INSERT INTO computadores (processador, ram, rom, grafica, produto_id) VALUES (Processador, Ram, Rom, Grafica, ID_PRODUTO(Nome,ID_VENDEDOR(nomeVendedor)));
+END;
+$$ LANGUAGE plpgsql;
+
 
 -- Users ============================================================================
 
 --Admins
-INSERT INTO utilizadores (username, password, nome) VALUES ('edini', 'password01', 'Eduardo Figueiredo');
-INSERT INTO admins (utilizador_id) VALUES (ID_USER('edini'));
-INSERT INTO utilizadores (username, password, nome) VALUES ('fabirino', 'password02', 'Fábio Santos');
-INSERT INTO admins (utilizador_id) VALUES (ID_USER('fabirino'));
+SELECT ADD_ADMIN('edini', 'password01', 'Eduardo Figueiredo');
+SELECT ADD_ADMIN('fabirino', 'password02', 'Fábio Santos');
 
 
 -- Compradores
-INSERT INTO utilizadores (username, password, nome) VALUES ('josem', 'password03', 'José Maria');
-INSERT INTO compradores (utilizador_id, morada) VALUES (ID_USER('josem'), 'Rua da Catraia, Lisboa');
-INSERT INTO utilizadores (username, password, nome) VALUES ('anamargarida', 'password04', 'Ana Margarida');
-INSERT INTO compradores (utilizador_id, morada) VALUES (ID_USER('anamargarida'), 'Avenida das Laranjeiras, Anadia');
+SELECT ADD_COMPRADOR('josem', 'password03', 'José Maria','Rua da Catraia, Lisboa');
+SELECT ADD_COMPRADOR('anamargarida', 'password04', 'Ana Margarida','Avenida das Laranjeiras, Anadia');
 
 -- Vendedores
-INSERT INTO utilizadores (username, password, nome) VALUES ('infortech', 'password05', 'InforTech LDA');
-INSERT INTO vendedores (utilizador_id, nif) VALUES (ID_USER('infortech'), 521376896);
-INSERT INTO utilizadores (username, password, nome) VALUES ('techmania', 'password06', 'TechMania');
-INSERT INTO vendedores (utilizador_id, nif) VALUES (ID_USER('techmania'), 624786152);
+SELECT ADD_VENDEDOR('infortech', 'password05', 'InforTech LDA',521376896);
+SELECT ADD_VENDEDOR('techmania', 'password06', 'TechMania',624786152);
 
 
 
--- Produtos =========================================================================
 
--- Computadores
+-- -- Produtos =========================================================================
 
-INSERT INTO produtos (nome, descricao, preco, stock, vendedor_id) VALUES ('Computador1', 'Intel i7 16GB RTX 3060', 1300, 3,ID_VENDEDOR('techmania'));
-INSERT INTO computadores (processador, ram, rom, grafica, produto_id) VALUES ('Intel i7 12700k', 16, 1024, 'RTX 3060', ID_PRODUTO('Computador1',ID_VENDEDOR('techmania')));
--- INSERT INTO produtos (nome, descricao, preco, stock, vendedor_id) VALUES ('Computador2', 'Ryzen9 5900x 32GB RTX 3090', 4000, 1,ID_VENDEDOR('infortech'));
-INSERT INTO computadores (processador, ram, rom, grafica, produto_id) VALUES ('Intel i7 12700k', 16, 1024, 'RTX 3060', ID_PRODUTO('Computador1',ID_VENDEDOR('infortech')));
-INSERT INTO produtos (nome, descricao, preco, stock, vendedor_id) VALUES ('Computador1', 'Intel i7 16GB RTX 3060', 1300, 3,ID_VENDEDOR('infortech'));
-
+-- -- Computadores
+SELECT ADD_COMPUTADOR('Computador1', 'Intel i7 16GB RTX 3060', 1300, 3,'techmania','Intel i7 12700k', 16, 1024, 'RTX 3060');
+SELECT ADD_COMPUTADOR('Computador1', 'Intel i7 16GB RTX 3060', 1300, 3,'infortech','Intel i7 12700k', 16, 1024, 'RTX 3060');
+SELECT ADD_COMPUTADOR('Computador2', 'Ryzen9 5900x 32GB RTX 3090', 4000, 1,'infortech','Ryzen9 5900x', 32, 1024, 'RTX 3090');
 
 -- Telemoveis
 
