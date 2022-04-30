7b29c2c03f789be876d85a6aa970bf7d40f491a0
FabioS <fabalexandre8@gmail.com>
2022-04-30 11:37:54 +0100

    GET methods added -> loja.py

--- src/loja.py
+++ src/loja.py
@@ -1,4 +1,5 @@
-import flask, psycopg2
+import flask
+import psycopg2
 import logging
 
 app = flask.Flask(__name__)
@@ -10,21 +11,169 @@ StatusCodes = {
 }
 
 # Acesso a DataBase
+
+
 def db_connection():
     db = psycopg2.connect(
-        user = "aulaspl",
-        password = "aulaspl",
-        host = "127.0.0.1", 
-        port = "5432",
-        database = "projetobd"
+        user="aulaspl",
+        password="aulaspl",
+        host="127.0.0.1",
+        port="5432",
+        database="projetobd"
     )
-    
+
     return db
 
+# ENDPOINTS =#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+# GET's ==========================================================================
+
+@app.route('/')
+def landing_page():
+    return """
+    Bem vindo à loja DataStore!
+    Comece as suas compras assim que possível :)
+    
+    Fábio Santos 2020212310
+    Eduardo Figueiredo 2020213717
+    """
 
 
+# http://localhost:8080/produtos/
+
+@app.route('/produtos/', methods=['GET'])
+def get_all_produts():
+    logger.info('GET /produtos')
+    conn = db_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute('SELECT * FROM produtos')
+        rows = cur.fetchall()
+
+        logger.debug('GET /produtos - parse')
+        Results = []
+        for row in rows:
+            logger.debug(row)
+            content = {'ID': int(row[0]), 'Nome': row[1], 'Descricao': row[2], 'Preco': int(
+                row[3]), 'Stock': int(row[4]), 'ID Vendedor': int(row[5])}
+            Results.append(content)
+
+        reponse = {'Status': StatusCodes['success'], 'Results': Results}
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'GET /produtos - error: {error}')
+        reponse = {
+            'Status': StatusCodes['internal_error'], 'Error': str(error)}
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(reponse)
 
 
+# http://localhost:8080/utilizadores/
+
+@app.route('/utilizadores/', methods=['GET'])
+def get_all_users():
+    logger.info('GET /utilizadores')
+    conn = db_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute('SELECT nome, id, username  FROM utilizadores')
+        rows = cur.fetchall()
+
+        logger.debug('GET /utilizadores - parse')
+        Results = []
+        for row in rows:
+            logger.debug(row)
+            content = {'Nome': row[0], 'ID': int(row[1]), 'Username': row[2]}
+            Results.append(content)
+
+        reponse = {'Status': StatusCodes['success'], 'results': Results}
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'GET /utilizadores - error: {error}')
+        reponse = {
+            'Status': StatusCodes['internal_error'], 'error': str(error)}
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(reponse)
+
+
+# http://localhost:8080/compradores/
+
+@app.route('/compradores/', methods=['GET'])
+def get_all_buyers():
+    logger.info('GET /compradores')
+    conn = db_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute('SELECT utilizadores.nome, utilizadores.id, utilizadores.username, compradores.morada FROM utilizadores, compradores WHERE utilizadores.id = compradores.utilizador_id')
+        rows = cur.fetchall()
+
+        logger.debug('GET /compradores - parse')
+        Results = []
+        for row in rows:
+            logger.debug(row)
+            content = {'Nome': row[0], 'ID': int(row[1]), 'Username': row[2], 'Morada': row[3]}
+            Results.append(content)
+
+        reponse = {'Status': StatusCodes['success'], 'results': Results}
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'GET /compradores - error: {error}')
+        reponse = {
+            'Status': StatusCodes['internal_error'], 'error': str(error)}
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(reponse)
+
+
+# http://localhost:8080/vendedores/
+
+@app.route('/vendedores/', methods=['GET'])
+def get_all_sellers():
+    logger.info('GET /compradores')
+    conn = db_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute('SELECT utilizadores.nome, utilizadores.id, utilizadores.username, vendedores.nif FROM utilizadores, vendedores WHERE utilizadores.id = vendedores.utilizador_id')
+        rows = cur.fetchall()
+
+        logger.debug('GET /vendedores - parse')
+        Results = []
+        for row in rows:
+            logger.debug(row)
+            content = {'Nome': row[0], 'ID': int(row[1]), 'Username': row[2], 'NIF': row[3]}
+            Results.append(content)
+
+        reponse = {'Status': StatusCodes['success'], 'results': Results}
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        logger.error(f'GET /vendedores - error: {error}')
+        reponse = {
+            'Status': StatusCodes['internal_error'], 'error': str(error)}
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(reponse)
+
+
+# POST's =========================================================================
+
+#TODO: 
 
 if __name__ == '__main__':
 
@@ -34,13 +183,12 @@ if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    
 
     # create formatter
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s]:  %(message)s', '%H:%M:%S')
+    formatter = logging.Formatter(
+        '%(asctime)s [%(levelname)s]:  %(message)s', '%H:%M:%S')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-    
 
     host = '127.0.0.1'
     port = 8080
